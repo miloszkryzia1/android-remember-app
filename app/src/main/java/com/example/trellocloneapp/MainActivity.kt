cf4c3ae67e8f78d4b1db920cea021af311e6efe5
miloszkryzia1 <milosz.kryzia@gmail.com>
2023-12-20 21:42:30 -0800

    add buttons and cancel button func in new board page

--- app/src/main/java/com/example/trellocloneapp/MainActivity.kt
+++ app/src/main/java/com/example/trellocloneapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.trellocloneapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Set fragment for most recent board
         // TODO: This is only for testing the layout, change in the future
         val boardFragment = BoardFragment()
         val args: Bundle = Bundle()
@@ -21,12 +24,15 @@ class MainActivity : AppCompatActivity() {
         boardFragment.arguments = args
         supportFragmentManager.beginTransaction().add(R.id.homePageFrame, boardFragment).commit()
 
+        //Navigation Drawer
         val drawer = findViewById<DrawerLayout>(R.id.mainDrawer)
-
         navMenuToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
-
         drawer.addDrawerListener(navMenuToggle)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //Button func
+        val btn = findViewById<Button>(R.id.homePageButton)
+        btn.setOnClickListener { openNewBoardScreen() }
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
@@ -39,5 +45,11 @@ class MainActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun openNewBoardScreen() {
+        intent = Intent(this, NewBoardActivity::class.java)
+        intent.putExtra("previous", "main")
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/trellocloneapp/NewBoardActivity.kt
+++ app/src/main/java/com/example/trellocloneapp/NewBoardActivity.kt
@@ -1,11 +1,50 @@
 package com.example.trellocloneapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 
 class NewBoardActivity : AppCompatActivity() {
+
+    private lateinit var navMenuToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_board)
+
+        //Nav menu toggle setup
+        val drawer = findViewById<DrawerLayout>(R.id.newBoardDrawer)
+        navMenuToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        drawer.addDrawerListener(navMenuToggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //Cancel button func
+        val cancelBtn = findViewById<Button>(R.id.cancelButton)
+        cancelBtn.setOnClickListener{ cancelButtonPressed() }
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        navMenuToggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (navMenuToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun cancelButtonPressed() {
+        when (intent.extras?.getString("previous")) {
+            "main" -> {
+                intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
