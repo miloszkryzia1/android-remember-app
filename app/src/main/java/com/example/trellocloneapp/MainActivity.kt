8cfacd642b6fb5fbd1daccecacbc37b292fe3474
miloszkryzia1 <milosz.kryzia@gmail.com>
2023-12-20 20:45:25 -0800

    finish design for nav menu

--- app/src/main/java/com/example/trellocloneapp/MainActivity.kt
+++ app/src/main/java/com/example/trellocloneapp/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.trellocloneapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navMenuToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +21,23 @@ class MainActivity : AppCompatActivity() {
         boardFragment.arguments = args
         supportFragmentManager.beginTransaction().add(R.id.homePageFrame, boardFragment).commit()
 
+        val drawer = findViewById<DrawerLayout>(R.id.mainDrawer)
 
+        navMenuToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+
+        drawer.addDrawerListener(navMenuToggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        navMenuToggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (navMenuToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
